0f41f54acf15f0059bfe0539fbc84d2439ff5aa9
Simon Blanke <simon.blanke@yahoo.com>
2021-05-23 21:05:31 +0200

    add plots to script + add multiple jpg

--- doc/create_readme_plots.py
+++ doc/create_readme_plots.py
@@ -9,6 +9,8 @@ from gradient_free_objective_functions.non_convex import (
     AckleyFunction,
     RosenbrockFunction,
     BealeFunction,
+    HimmelblausFunction,
+    HölderTableFunction,
 )
 from gradient_free_objective_functions.visualize import (
     matplotlib_heatmap,
@@ -23,57 +25,63 @@ rastrigin_function = RastriginFunction(2, metric="loss")
 ackley_function = AckleyFunction(metric="loss")
 rosenbrock_function = RosenbrockFunction(metric="loss")
 beale_function = BealeFunction(metric="loss")
+himmelblaus_function = HimmelblausFunction(metric="loss")
+hölder_table_function = HölderTableFunction(metric="loss")
 
 
-objective_functions = [
-    sphere_function,
-    rastrigin_function,
-    ackley_function,
-    rosenbrock_function,
-    beale_function,
-]
+resolution = 0.01
 
-search_space = {
-    "x0": np.arange(-5, 5, 0.01),
-    "x1": np.arange(-5, 5, 0.01),
+search_space1 = {
+    "x0": np.arange(-5, 5, resolution),
+    "x1": np.arange(-5, 5, resolution),
+}
+
+search_space2 = {
+    "x0": np.arange(-10, 10, resolution),
+    "x1": np.arange(-10, 10, resolution),
 }
 
 
 objective_function_infos = {
     sphere_function: {
-        "name": "sphere_function",
-        "search_space": search_space,
+        "search_space": search_space1,
         "norm": None,
     },
     rastrigin_function: {
-        "name": "rastrigin_function",
-        "search_space": search_space,
+        "search_space": search_space1,
         "norm": None,
     },
     ackley_function: {
-        "name": "ackley_function",
-        "search_space": search_space,
+        "search_space": search_space1,
         "norm": None,
     },
     rosenbrock_function: {
-        "name": "rosenbrock_function",
-        "search_space": search_space,
+        "search_space": search_space1,
         "norm": None,
     },
     beale_function: {
-        "name": "beale_function",
-        "search_space": search_space,
+        "search_space": search_space1,
         "norm": "color_log",
+    },
+    himmelblaus_function: {
+        "search_space": search_space1,
+        "norm": "color_log",
+    },
+    hölder_table_function: {
+        "search_space": search_space2,
+        "norm": None,
     },
 }
 
 
-for objective_function in objective_functions:
+for objective_function in objective_function_infos.keys():
     objective_function_info = objective_function_infos[objective_function]
 
-    name = objective_function_info["name"]
+    name = objective_function.__name__
     search_space = objective_function_info["search_space"]
     norm = objective_function_info["norm"]
+
+    print(name, "\n")
 
     matplotlib_heatmap(objective_function, search_space, norm=norm).savefig(
         path + "/images/" + name + "_heatmap.jpg", dpi=100
