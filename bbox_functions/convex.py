d2e8021524c9f092c449cee9e9c833ea620c01b9
Simon Blanke <simon.blanke@yahoo.com>
2021-06-02 20:00:29 +0200

    add sympy equation + parameters to class

--- bbox_functions/convex.py
+++ bbox_functions/convex.py
@@ -2,13 +2,26 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
+from sympy import Sum, Indexed
+from sympy.abc import x, i
+
+
 from .base_objective_function import ObjectiveFunction
 
 
 class SphereFunction(ObjectiveFunction):
+    name = "sphere_function"
+    continuous = True
+    convex = True
+    separable = True
+    differentiable = False
+    mutimodal = False
+    randomized_term = False
+    parametric = False
+
     def __init__(self, n_dim, A=1, metric="score", input_type="dictionary"):
         super().__init__(metric, input_type)
-        self.__name__ = "sphere_function"
+        self.equation = Sum(Indexed(x, i) ** 2, (i, 1, n_dim))
 
         self.n_dim = n_dim
         self.A = A
