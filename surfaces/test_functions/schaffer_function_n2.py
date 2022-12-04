1d44e326220639a93eb7efc6824e265ecec9f46e
Simon Blanke <simon.blanke@yahoo.com>
2022-12-04 20:21:02 +0100

    add Schaffer Function N2

---
+++ surfaces/test_functions/schaffer_function_n2.py
@@ -0,0 +1,26 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+import numpy as np
+
+from ._base_objective_function import ObjectiveFunction
+
+
+class SchafferFunctionN2(ObjectiveFunction):
+    name = "Schaffer Function N2"
+    _name_ = "schaffer_function_n2"
+    __name__ = "SchafferFunctionN2"
+
+    def __init__(self, metric="score", input_type="dictionary", sleep=0):
+        super().__init__(metric, input_type, sleep)
+        self.n_dim = 2
+
+    def objective_function_dict(self, params):
+        x = params["x0"]
+        y = params["x1"]
+
+        loss = 100 * np.sqrt(np.abs(y - 0.01 * x ** 2)) + 0.01 * np.abs(x + 10)
+
+        return self.return_metric(loss)
