50c564d6b7325a399f26d517647c51fa32cc32b6
Simon Blanke <simon.blanke@yahoo.com>
2023-01-10 18:52:57 +0100

    add Griewank Function

---
+++ surfaces/test_functions/griewank_function.py
@@ -0,0 +1,31 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+import numpy as np
+
+from ._base_objective_function import ObjectiveFunction
+
+
+class GriewankFunction(ObjectiveFunction):
+    name = "Griewank Function"
+    _name_ = "griewank_function"
+    __name__ = "GriewankFunction"
+
+    def __init__(self, *args, n_dim, **kwargs):
+        super().__init__(args, **kwargs)
+        self.n_dim = n_dim
+
+    def objective_function_dict(self, params):
+        loss_sum = 0
+        loss_product = 1
+        for dim in range(self.n_dim):
+            dim_str = "x" + str(dim)
+            x = params[dim_str]
+
+            loss_sum += x ** 2 / 4000
+            loss_product *= np.cos(x / np.sqrt(dim + 1))
+
+        loss = loss_sum - loss_product + 1
+        return self.return_metric(loss)
