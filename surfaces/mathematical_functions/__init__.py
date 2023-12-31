d323138edfe85ae41dd7a5b5cac99d50c00beb3c
Simon Blanke <simon.blanke@yahoo.com>
2023-12-31 14:43:09 +0100

    add optimizer classes to init-file

--- surfaces/mathematical_functions/__init__.py
+++ surfaces/mathematical_functions/__init__.py
@@ -3,57 +3,81 @@
 # License: MIT License
 
 
-from .sphere_function import SphereFunction
 from .ackley_function import AckleyFunction
-from .rastrigin_function import RastriginFunction
-from .rosenbrock_function import RosenbrockFunction
 from .beale_function import BealeFunction
+from .booth_function import BoothFunction
+from .bukin_function_n6 import BukinFunctionN6
+from .cross_in_tray_function import CrossInTrayFunction
+from .drop_wave_function import DropWaveFunction
+from .easom_function import EasomFunction
+from .eggholder_function import EggholderFunction
+from .goldstein_price_function import GoldsteinPriceFunction
+from .gramacy_and_lee_function import GramacyAndLeeFunction
+from .griewank_function import GriewankFunction
 from .himmelblaus_function import HimmelblausFunction
 from .hoelder_table_function import HölderTableFunction
-from .cross_in_tray_function import CrossInTrayFunction
-from .simionescu_function import SimionescuFunction
-from .easom_function import EasomFunction
-from .booth_function import BoothFunction
-from .goldstein_price_function import GoldsteinPriceFunction
-from .styblinski_tang_function import StyblinskiTangFunction
+from .langermann_function import LangermannFunction
+from .levi_function_n13 import LeviFunctionN13
 from .matyas_function import MatyasFunction
 from .mccormick_function import McCormickFunction
-from .bukin_function_n6 import BukinFunctionN6
+from .rastrigin_function import RastriginFunction
+from .rosenbrock_function import RosenbrockFunction
+from .schaffer_function_n2 import SchafferFunctionN2
+from .simionescu_function import SimionescuFunction
+from .sphere_function import SphereFunction
+from .styblinski_tang_function import StyblinskiTangFunction
+from .three_hump_camel_function import ThreeHumpCamelFunction
 
 __all__ = [
-    "SphereFunction",
     "AckleyFunction",
-    "RastriginFunction",
-    "RosenbrockFunction",
     "BealeFunction",
+    "BoothFunction",
+    "BukinFunctionN6",
+    "CrossInTrayFunction",
+    "DropWaveFunction",
+    "EasomFunction",
+    "EggholderFunction",
+    "GoldsteinPriceFunction",
+    "GramacyAndLeeFunction",
+    "GriewankFunction",
     "HimmelblausFunction",
     "HölderTableFunction",
-    "CrossInTrayFunction",
-    "SimionescuFunction",
-    "EasomFunction",
-    "BoothFunction",
-    "GoldsteinPriceFunction",
-    "StyblinskiTangFunction",
+    "LangermannFunction",
+    "LeviFunctionN13",
     "MatyasFunction",
     "McCormickFunction",
-    "BukinFunctionN6",
+    "RastriginFunction",
+    "RosenbrockFunction",
+    "SchafferFunctionN2",
+    "SimionescuFunction",
+    "SphereFunction",
+    "StyblinskiTangFunction",
+    "ThreeHumpCamelFunction",
 ]
 
 all = [
-    SphereFunction,
     AckleyFunction,
-    RastriginFunction,
-    RosenbrockFunction,
     BealeFunction,
+    BoothFunction,
+    BukinFunctionN6,
+    CrossInTrayFunction,
+    DropWaveFunction,
+    EasomFunction,
+    EggholderFunction,
+    GoldsteinPriceFunction,
+    GramacyAndLeeFunction,
+    GriewankFunction,
     HimmelblausFunction,
     HölderTableFunction,
-    CrossInTrayFunction,
-    SimionescuFunction,
-    EasomFunction,
-    BoothFunction,
-    GoldsteinPriceFunction,
-    StyblinskiTangFunction,
+    LangermannFunction,
+    LeviFunctionN13,
     MatyasFunction,
     McCormickFunction,
-    BukinFunctionN6,
+    RastriginFunction,
+    RosenbrockFunction,
+    SchafferFunctionN2,
+    SimionescuFunction,
+    SphereFunction,
+    StyblinskiTangFunction,
+    ThreeHumpCamelFunction,
 ]
