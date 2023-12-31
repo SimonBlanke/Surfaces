0b537b65b2beeebd44a3bf6b98fc524062af8c66
Simon Blanke <simon.blanke@yahoo.com>
2023-12-31 14:52:21 +0100

    change database path

--- surfaces/data_collector/config.py
+++ surfaces/data_collector/config.py
@@ -4,5 +4,5 @@ here_path = os.path.dirname(os.path.realpath(__file__))
 
 
 paths = {
-    "machine learning data path": os.path.join(here_path, "..", "machine_learning.db"),
+    "search-data path": os.path.join(here_path, "..", "search_data.db"),
 }
--- surfaces/data_collector/surfaces_data_collector.py
+++ surfaces/data_collector/surfaces_data_collector.py
@@ -5,5 +5,5 @@ from search_data_collector import SqlDataCollector
 
 class SurfacesDataCollector(SqlDataCollector):
     def __init__(self) -> None:
-        path = paths["machine learning data path"]
+        path = paths["search-data path"]
         super().__init__(path, func2str=True)
