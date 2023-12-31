47354c81e44aeeb7dc41828f3371a426e4799cec
Simon Blanke <simon.blanke@yahoo.com>
2023-12-31 15:55:53 +0100

    add script that collects search-data into sql database

---
+++ collect_search_data.py
@@ -0,0 +1,16 @@
+from surfaces import mathematical_functions, machine_learning_functions
+
+for mathematical_function in mathematical_functions:
+    print("\n ---", mathematical_function.name, "---")
+    try:
+        test_function = mathematical_function()
+    except TypeError:
+        test_function = mathematical_function(n_dim=2)
+
+    test_function.collect_data()
+
+"""
+for machine_learning_function in machine_learning_functions:
+    test_function = machine_learning_function()
+    test_function.collect_data()
+"""
