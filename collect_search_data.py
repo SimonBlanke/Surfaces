b926b102922a7073e3803563e1c3d63fe5fde970
Simon Blanke <simon.blanke@yahoo.com>
2024-01-02 09:50:44 +0100

    eval loss of math. test functions instead of score

--- collect_search_data.py
+++ collect_search_data.py
@@ -1,16 +1,16 @@
 from surfaces import mathematical_functions, machine_learning_functions
 
+
 for mathematical_function in mathematical_functions:
     print("\n ---", mathematical_function.name, "---")
     try:
-        test_function = mathematical_function()
+        test_function = mathematical_function(metric="loss")
     except TypeError:
-        test_function = mathematical_function(n_dim=2)
+        test_function = mathematical_function(n_dim=2, metric="loss")
 
     test_function.collect_data()
 
-"""
+
 for machine_learning_function in machine_learning_functions:
     test_function = machine_learning_function()
     test_function.collect_data()
-"""
