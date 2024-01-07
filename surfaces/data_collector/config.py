dc2ccdc61f39c4c23e3dcc942ce79444c94d7e52
Simon Blanke <simon.blanke@yahoo.com>
2024-01-07 16:04:50 +0100

    use abspath

--- surfaces/data_collector/config.py
+++ surfaces/data_collector/config.py
@@ -4,5 +4,7 @@ here_path = os.path.dirname(os.path.realpath(__file__))
 
 
 paths = {
-    "search-data path": os.path.join(here_path, "..", "search_data.db"),
+    "search-data path": os.path.abspath(
+        os.path.join(here_path, "..", "search_data.db")
+    ),
 }
