1e0bf5bdc27da7b1aa90b3bd118c012e2e723b75
Simon Blanke <simon.blanke@yahoo.com>
2024-01-18 18:36:37 +0100

    separate 1d,2d,nd test-functions into directories

--- surfaces/mathematical_functions/__init__.py
+++ surfaces/mathematical_functions/__init__.py
@@ -3,30 +3,35 @@
 # License: MIT License
 
 
-from .ackley_function import AckleyFunction
-from .beale_function import BealeFunction
-from .booth_function import BoothFunction
-from .bukin_function_n6 import BukinFunctionN6
-from .cross_in_tray_function import CrossInTrayFunction
-from .drop_wave_function import DropWaveFunction
-from .easom_function import EasomFunction
-from .eggholder_function import EggholderFunction
-from .goldstein_price_function import GoldsteinPriceFunction
-from .gramacy_and_lee_function import GramacyAndLeeFunction
-from .griewank_function import GriewankFunction
-from .himmelblaus_function import HimmelblausFunction
-from .hoelder_table_function import HölderTableFunction
-from .langermann_function import LangermannFunction
-from .levi_function_n13 import LeviFunctionN13
-from .matyas_function import MatyasFunction
-from .mccormick_function import McCormickFunction
-from .rastrigin_function import RastriginFunction
-from .rosenbrock_function import RosenbrockFunction
-from .schaffer_function_n2 import SchafferFunctionN2
-from .simionescu_function import SimionescuFunction
-from .sphere_function import SphereFunction
-from .styblinski_tang_function import StyblinskiTangFunction
-from .three_hump_camel_function import ThreeHumpCamelFunction
+from .test_functions_1d.gramacy_and_lee_function import GramacyAndLeeFunction
+
+
+from .test_functions_2d.ackley_function import AckleyFunction
+from .test_functions_2d.beale_function import BealeFunction
+from .test_functions_2d.booth_function import BoothFunction
+from .test_functions_2d.bukin_function_n6 import BukinFunctionN6
+from .test_functions_2d.cross_in_tray_function import CrossInTrayFunction
+from .test_functions_2d.drop_wave_function import DropWaveFunction
+from .test_functions_2d.easom_function import EasomFunction
+from .test_functions_2d.eggholder_function import EggholderFunction
+from .test_functions_2d.goldstein_price_function import GoldsteinPriceFunction
+from .test_functions_2d.griewank_function import GriewankFunction
+from .test_functions_2d.himmelblaus_function import HimmelblausFunction
+from .test_functions_2d.hoelder_table_function import HölderTableFunction
+from .test_functions_2d.langermann_function import LangermannFunction
+from .test_functions_2d.levi_function_n13 import LeviFunctionN13
+from .test_functions_2d.matyas_function import MatyasFunction
+from .test_functions_2d.mccormick_function import McCormickFunction
+from .test_functions_2d.schaffer_function_n2 import SchafferFunctionN2
+from .test_functions_2d.simionescu_function import SimionescuFunction
+from .test_functions_2d.three_hump_camel_function import ThreeHumpCamelFunction
+
+
+from .test_functions_nd.rastrigin_function import RastriginFunction
+from .test_functions_nd.rosenbrock_function import RosenbrockFunction
+from .test_functions_nd.sphere_function import SphereFunction
+from .test_functions_nd.styblinski_tang_function import StyblinskiTangFunction
+
 
 __all__ = [
     "AckleyFunction",
---
+++ surfaces/mathematical_functions/test_functions_nd/__init__.py
@@ -0,0 +1,16 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+from .rastrigin_function import RastriginFunction
+from .rosenbrock_function import RosenbrockFunction
+from .sphere_function import SphereFunction
+from .styblinski_tang_function import StyblinskiTangFunction
+
+__all__ = [
+    "RastriginFunction",
+    "RosenbrockFunction",
+    "SphereFunction",
+    "StyblinskiTangFunction",
+]
