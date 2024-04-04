7cf37636568b98b06dbafb1b7ede80e7e55834f2
Simon Blanke <simon.blanke@yahoo.com>
2024-04-04 08:32:47 +0200

    add search-space method to machine-learning test functions

--- src/surfaces/machine_learning_functions/tabular_classifiers.py
+++ src/surfaces/machine_learning_functions/tabular_classifiers.py
@@ -13,18 +13,35 @@ class KNeighborsClassifierFunction(MachineLearningFunction):
     _name_ = "k_neighbors_classifier"
     __name__ = "KNeighborsClassifierFunction"
 
-    def __init__(self, input_type="dictionary", sleep=0):
-        super().__init__()
+    para_names = ["n_neighbors", "algorithm", "cv", "dataset"]
 
-        self.input_type = input_type
-        self.sleep = sleep
+    def __init__(self, metric=None, sleep=0):
+        super().__init__(metric, sleep)
 
-        self.search_space = {
-            "n_neighbors": list(np.arange(3, 150, 5)),
-            "algorithm": ["auto", "ball_tree", "kd_tree", "brute"],
-            "cv": [2, 3, 4, 5, 8, 10],
-            "dataset": [digits_data, wine_data, iris_data],
-        }
+    def search_space(
+        self,
+        n_neighbors: list = None,
+        algorithm: list = None,
+        cv: list = None,
+        dataset: list = None,
+    ):
+        search_space: dict = {}
+
+        n_neighbors_default = list(np.arange(3, 150, 5))
+        algorithm_default = ["auto", "ball_tree", "kd_tree", "brute"]
+        cv_default = [2, 3, 4, 5, 8, 10]
+        dataset_default = [digits_data, wine_data, iris_data]
+
+        search_space["n_neighbors"] = (
+            n_neighbors_default if n_neighbors is None else n_neighbors
+        )
+        search_space["algorithm"] = (
+            algorithm_default if algorithm is None else algorithm
+        )
+        search_space["cv"] = cv_default if cv is None else cv
+        search_space["dataset"] = dataset_default if dataset is None else dataset
+
+        return search_space
 
     def create_objective_function(self):
         def k_neighbors_classifier(params):
@@ -33,7 +50,7 @@ class KNeighborsClassifierFunction(MachineLearningFunction):
                 algorithm=params["algorithm"],
             )
             X, y = params["dataset"]()
-            scores = cross_val_score(knc, X, y, cv=params["cv"])
+            scores = cross_val_score(knc, X, y, cv=params["cv"], scoring=self.metric)
             return scores.mean()
 
         self.pure_objective_function = k_neighbors_classifier
--- src/surfaces/machine_learning_functions/tabular_regressors.py
+++ src/surfaces/machine_learning_functions/tabular_regressors.py
@@ -14,27 +14,44 @@ class KNeighborsRegressorFunction(MachineLearningFunction):
     _name_ = "k_neighbors_regressor"
     __name__ = "KNeighborsRegressorFunction"
 
-    def __init__(self, input_type="dictionary", sleep=0):
-        super().__init__()
+    para_names = ["n_neighbors", "algorithm", "cv", "dataset"]
 
-        self.input_type = input_type
-        self.sleep = sleep
+    def __init__(self, metric=None, sleep=0):
+        super().__init__(metric, sleep)
 
-        self.search_space = {
-            "n_neighbors": list(np.arange(3, 150, 5)),
-            "algorithm": ["auto", "ball_tree", "kd_tree", "brute"],
-            "cv": [2, 3, 4, 5, 10],
-            "dataset": [diabetes_data],
-        }
+    def search_space(
+        self,
+        n_neighbors: list = None,
+        algorithm: list = None,
+        cv: list = None,
+        dataset: list = None,
+    ):
+        search_space: dict = {}
+
+        n_neighbors_default = list(np.arange(3, 150, 5))
+        algorithm_default = ["auto", "ball_tree", "kd_tree", "brute"]
+        cv_default = [2, 3, 4, 5, 8, 10]
+        dataset_default = [diabetes_data]
+
+        search_space["n_neighbors"] = (
+            n_neighbors_default if n_neighbors is None else n_neighbors
+        )
+        search_space["algorithm"] = (
+            algorithm_default if algorithm is None else algorithm
+        )
+        search_space["cv"] = cv_default if cv is None else cv
+        search_space["dataset"] = dataset_default if dataset is None else dataset
+
+        return search_space
 
     def create_objective_function(self):
         def k_neighbors_regressor(params):
             knc = KNeighborsRegressor(
-                n_neighbors=params["n_neighbors"],
-                algorithm=params["algorithm"],
+                n_estimators=params["n_estimators"],
+                max_depth=params["max_depth"],
             )
             X, y = params["dataset"]()
-            scores = cross_val_score(knc, X, y, cv=params["cv"])
+            scores = cross_val_score(knc, X, y, cv=params["cv"], scoring=self.metric)
             return scores.mean()
 
         self.pure_objective_function = k_neighbors_regressor
@@ -45,18 +62,35 @@ class GradientBoostingRegressorFunction(MachineLearningFunction):
     _name_ = "gradient_boosting_regressor"
     __name__ = "GradientBoostingRegressorFunction"
 
-    def __init__(self, input_type="dictionary", sleep=0):
-        super().__init__()
+    para_names = ["n_estimators", "max_depth", "cv", "dataset"]
 
-        self.input_type = input_type
-        self.sleep = sleep
+    def __init__(self, metric=None, sleep=0):
+        super().__init__(metric, sleep)
 
-        self.search_space = {
-            "n_estimators": list(np.arange(5, 150, 5)),
-            "max_depth": list(np.arange(1, 15)),
-            "cv": [2, 3, 4, 5],
-            "dataset": [diabetes_data],
-        }
+    def search_space(
+        self,
+        n_estimators: list = None,
+        max_depth: list = None,
+        cv: list = None,
+        dataset: list = None,
+    ):
+        search_space: dict = {}
+
+        n_estimators_default = list(np.arange(3, 150, 5))
+        max_depth_default = ["auto", "ball_tree", "kd_tree", "brute"]
+        cv_default = [2, 3, 4, 5, 8, 10]
+        dataset_default = [diabetes_data]
+
+        search_space["n_estimators"] = (
+            n_estimators_default if n_estimators is None else n_estimators
+        )
+        search_space["max_depth"] = (
+            max_depth_default if max_depth is None else max_depth
+        )
+        search_space["cv"] = cv_default if cv is None else cv
+        search_space["dataset"] = dataset_default if dataset is None else dataset
+
+        return search_space
 
     def create_objective_function(self):
         def gradient_boosting_regressor(params):
@@ -65,7 +99,7 @@ class GradientBoostingRegressorFunction(MachineLearningFunction):
                 max_depth=params["max_depth"],
             )
             X, y = params["dataset"]()
-            scores = cross_val_score(knc, X, y, cv=params["cv"])
+            scores = cross_val_score(knc, X, y, cv=params["cv"], scoring=self.metric)
             return scores.mean()
 
         self.pure_objective_function = gradient_boosting_regressor
