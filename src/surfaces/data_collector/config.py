2e7d649fdb137b5f234167a7839f75ff77fc6352
Simon Blanke <simon.blanke@yahoo.com>
2024-04-20 10:04:26 +0200

    add default para for surfaces-sdc

--- src/surfaces/data_collector/config.py
+++ src/surfaces/data_collector/config.py
@@ -1,10 +1,6 @@
 import os
 
 here_path = os.path.dirname(os.path.realpath(__file__))
-
-
-paths = {
-    "search-data path": os.path.abspath(
-        os.path.join(here_path, "..", "search_data.db")
-    ),
-}
+default_search_data_path = os.path.abspath(
+    os.path.join(here_path, "..", "search_data.db")
+)
--- src/surfaces/data_collector/surfaces_data_collector.py
+++ src/surfaces/data_collector/surfaces_data_collector.py
@@ -1,9 +1,8 @@
 from search_data_collector import SqlSearchData
 
-from .config import paths
+from .config import default_search_data_path
 
 
 class SurfacesDataCollector(SqlSearchData):
-    def __init__(self) -> None:
-        path = paths["search-data path"]
+    def __init__(self, path=default_search_data_path) -> None:
         super().__init__(path, func2str=True)
