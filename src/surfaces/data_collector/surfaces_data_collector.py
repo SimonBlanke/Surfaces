6ba1e53bc5eb1a23873bbf3be248800b5f7e6b09
Simon Blanke <simon.blanke@yahoo.com>
2024-04-20 10:10:57 +0200

    change default arg to None

--- src/surfaces/data_collector/surfaces_data_collector.py
+++ src/surfaces/data_collector/surfaces_data_collector.py
@@ -4,5 +4,7 @@ from .config import default_search_data_path
 
 
 class SurfacesDataCollector(SqlSearchData):
-    def __init__(self, path=default_search_data_path) -> None:
+    def __init__(self, path=None) -> None:
+        if path is None:
+            path = default_search_data_path
         super().__init__(path, func2str=True)
