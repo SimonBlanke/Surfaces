24025d55e8f963423f3c39d2513325a0564d3314
Simon Blanke <simon.blanke@yahoo.com>
2024-05-26 12:41:37 +0200

    remove unused import

--- collect_search_data.py
+++ collect_search_data.py
@@ -1,26 +1,12 @@
-from surfaces.test_functions import mathematical_functions, machine_learning_functions
+from surfaces.test_functions import machine_learning_functions
 from surfaces.data_collector import SurfacesDataCollector
 
 
-for mathematical_function in mathematical_functions:
-    print("\n ---", mathematical_function.name, "---")
-    try:
-        test_function = mathematical_function(metric="loss")
-    except TypeError:
-        test_function = mathematical_function(n_dim=2, metric="loss")
-
-    objective_function = test_function.objective_function
-    search_space = test_function.search_space(value_types="array")
-
-    sdc = SurfacesDataCollector()
-    sdc.collect(objective_function, search_space)
-
-
 for machine_learning_function in machine_learning_functions:
-    test_function = machine_learning_function(load_search_data=False)
+    test_function = machine_learning_function(evaluate_from_data=False)
 
     objective_function = test_function.objective_function
-    search_space = test_function.search_space(value_types="array")
+    search_space = test_function.search_space()
 
     sdc = SurfacesDataCollector()
     sdc.collect(objective_function, search_space)
