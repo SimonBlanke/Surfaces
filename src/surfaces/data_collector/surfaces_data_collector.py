c465107f1f66e185a39787659ef4a6d4abcda5de
Simon Blanke <simon.blanke@yahoo.com>
2025-07-20 09:49:24 +0200

    add '_perform_grid_search'-method

--- src/surfaces/data_collector/surfaces_data_collector.py
+++ src/surfaces/data_collector/surfaces_data_collector.py
@@ -4,14 +4,10 @@
 
 
 import numpy as np
-import pandas as pd
 from functools import reduce
 
 from search_data_collector import SqlSearchData
-from hyperactive import Hyperactive
-from hyperactive.optimizers import GridSearchOptimizer as HyperactiveGridSearchOptimizer
-from gradient_free_optimizers import GridSearchOptimizer
-
+from .grid_search import GridSearchOptimizer, DataFrame, SearchData
 from .config import default_search_data_path
 
 
@@ -30,6 +26,46 @@ class SurfacesDataCollector(SqlSearchData):
 
         search_data_cols = self.para_names + ["score"]
         self.search_data = pd.DataFrame([], columns=search_data_cols)
+
+    def _perform_grid_search(self, objective_function, search_space):
+        """
+        Perform grid search using our custom implementation.
+        This method handles both array and list search spaces uniformly.
+        """
+        while self.search_data_length < self.search_space_size:
+            # Create optimizer with current search space
+            opt = GridSearchOptimizer(
+                search_space,
+                direction="orthogonal",
+            )
+
+            # Create memory warm start if we have existing data
+            memory_warm_start = None
+            if len(self.search_data) > 0:
+                memory_warm_start = SearchData(self.para_names)
+                for _, row in self.search_data.iterrows():
+                    memory_warm_start.add_entry(row)
+
+            # Run the search
+            opt.search(
+                objective_function,
+                n_iter=int(self.search_space_size * 1),
+                verbosity=["progress_bar"],
+                memory_warm_start=memory_warm_start,
+            )
+
+            # Convert search data to DataFrame format
+            opt_df = DataFrame(opt.search_data.data, columns=opt.search_data.columns)
+
+            # Concatenate with existing data
+            self.search_data = DataFrame.concat(
+                [self.search_data, opt_df],
+                ignore_index=True,
+            )
+
+            # Remove duplicates
+            self.search_data = self.search_data.drop_duplicates(subset=self.para_names)
+            self.search_data_length = len(self.search_data)
 
     def _array_search_space(self, objective_function, search_space):
         while self.search_data_length < self.search_space_size:
