627047a5ed7fd423adb550ce4b2cc0ae82d6380f
Simon Blanke <simon.blanke@yahoo.com>
2025-08-09 11:21:48 +0200

    updatge script to new api design

--- collect_search_data.py
+++ collect_search_data.py
@@ -5,8 +5,6 @@ from surfaces.data_collector import SurfacesDataCollector
 for machine_learning_function in machine_learning_functions:
     test_function = machine_learning_function(evaluate_from_data=False)
 
-    objective_function = test_function.objective_function
-    search_space = test_function.search_space()
-
     sdc = SurfacesDataCollector()
-    sdc.collect(objective_function, search_space)
+    # Now uses default search space automatically and handles dataset objects
+    sdc.collect_search_data(test_function)
