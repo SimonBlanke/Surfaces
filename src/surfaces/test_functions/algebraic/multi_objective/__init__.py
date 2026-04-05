834dcf04dfd6ba40a56a16655db5b73f5a11267d
Simon Blanke <simon.blanke@yahoo.com>
2026-04-05 16:43:41 +0200

    add MO test functions to init files

--- src/surfaces/test_functions/algebraic/multi_objective/__init__.py
+++ src/surfaces/test_functions/algebraic/multi_objective/__init__.py
@@ -5,12 +5,14 @@
 """Multi-objective optimization test functions.
 
 This module provides benchmark functions for multi-objective optimization,
-where the goal is to find the Pareto front - the set of solutions where
+where the goal is to find the Pareto front -- the set of solutions where
 no objective can be improved without worsening another.
 
-Available Functions
--------------------
-ZDT1 : Convex Pareto front, scalable dimensions
+Available Families
+------------------
+ZDT : Bi-objective problems with various Pareto front geometries (ZDT1-4, ZDT6)
+DTLZ : Scalable problems with configurable number of objectives (DTLZ1-7)
+WFG : Transformation-based problems with configurable pipelines (WFG1-9)
 FonsecaFleming : Non-convex Pareto front, scalable dimensions
 Kursawe : Disconnected Pareto front, scalable dimensions
 
@@ -22,27 +24,79 @@ Examples
 >>> result.shape
 (2,)
 
->>> # Get the theoretical Pareto front
+>>> from surfaces.test_functions.algebraic.multi_objective import DTLZ2
+>>> func = DTLZ2(n_objectives=3)
 >>> front = func.pareto_front(n_points=100)
 >>> front.shape
-(100, 2)
+(100, 3)
 """
 
 from ._base_multi_objective import BaseMultiObjectiveTestFunction, MultiObjectiveFunction
+from .dtlz import (
+    DTLZ1,
+    DTLZ2,
+    DTLZ3,
+    DTLZ4,
+    DTLZ5,
+    DTLZ6,
+    DTLZ7,
+    dtlz_functions,
+)
 from .fonseca_fleming import FonsecaFleming
 from .kursawe import Kursawe
-from .zdt1 import ZDT1
+from .wfg import (
+    WFG1,
+    WFG2,
+    WFG3,
+    WFG4,
+    WFG5,
+    WFG6,
+    WFG7,
+    WFG8,
+    WFG9,
+    wfg_functions,
+)
+from .zdt import (
+    ZDT1,
+    ZDT2,
+    ZDT3,
+    ZDT4,
+    ZDT6,
+    zdt_functions,
+)
 
 __all__ = [
     "BaseMultiObjectiveTestFunction",
     "MultiObjectiveFunction",
+    # ZDT
     "ZDT1",
+    "ZDT2",
+    "ZDT3",
+    "ZDT4",
+    "ZDT6",
+    # DTLZ
+    "DTLZ1",
+    "DTLZ2",
+    "DTLZ3",
+    "DTLZ4",
+    "DTLZ5",
+    "DTLZ6",
+    "DTLZ7",
+    # WFG
+    "WFG1",
+    "WFG2",
+    "WFG3",
+    "WFG4",
+    "WFG5",
+    "WFG6",
+    "WFG7",
+    "WFG8",
+    "WFG9",
+    # Others
     "FonsecaFleming",
     "Kursawe",
 ]
 
-multi_objective_functions = [
-    ZDT1,
-    FonsecaFleming,
-    Kursawe,
-]
+multi_objective_functions = (
+    zdt_functions + dtlz_functions + wfg_functions + [FonsecaFleming, Kursawe]
+)
